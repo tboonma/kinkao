75aca7df96f53f303d4c5359b6c4bef93f26fad2
tboonma <tboonma@hotmail.com>
2023-03-29 01:01:27 +0700

    Add some input validation

--- src/main/java/ku/kinkao/controller/SignupController.java
+++ src/main/java/ku/kinkao/controller/SignupController.java
@@ -5,9 +5,12 @@ import ku.kinkao.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class SignupController {
@@ -15,12 +18,15 @@ public class SignupController {
     private SignupService signupService;
 
     @GetMapping("/signup")
-    public String getSignupPage() {
+    public String getSignupPage(SignupDto user) {
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String signupMember(@ModelAttribute SignupDto user, Model model) {
+    public String signupMember(@Valid SignupDto user, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "signup";
+
         String signupError = null;
 
         if (!signupService.isUsernameAvailable(user.getUsername())) {
@@ -28,17 +34,13 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            int rowsAdded = signupService.createUser(user);
-            if (rowsAdded < 0) {
-                signupError = "There was an error signing you up. Please try again.";
-            }
-        }
-
-        if (signupError == null) {
+            signupService.createUser(user);
             model.addAttribute("signupSuccess", true);
         } else {
             model.addAttribute("signupError", signupError);
         }
+
+        model.addAttribute("signupDto", new SignupDto());
 
         return "signup";
     }
