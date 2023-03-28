33c0b12534303de6babc2e6a75ad2621b8073f65
tboonma <tboonma@hotmail.com>
2023-03-28 14:44:06 +0700

    Initial project

---
+++ src/main/java/ku/kinkao/repository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package ku.kinkao.repository;
+
+import ku.kinkao.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+}
