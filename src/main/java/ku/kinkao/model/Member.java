6253cfcf95cf8f5af5967a3572c307c1b81939b8
tboonma <tboonma@hotmail.com>
2023-03-28 21:28:19 +0700

    Add Random ID to Model classes

--- src/main/java/ku/kinkao/model/Member.java
+++ src/main/java/ku/kinkao/model/Member.java
@@ -5,16 +5,16 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue
+    private UUID id;
 
     private String username;
     private String password;
--- src/main/java/ku/kinkao/model/Restaurant.java
+++ src/main/java/ku/kinkao/model/Restaurant.java
@@ -5,16 +5,16 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @Entity
 public class Restaurant {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue
+    private UUID id;
 
     private String name;
     private String address;
