ad435ec675322fe702766bf1257377129d40fe07
tboonma <tboonma@hotmail.com>
2023-03-31 16:05:21 +0700

    Add validation to RestaurantRequest

--- src/main/java/ku/kinkao/controller/RestaurantController.java
+++ src/main/java/ku/kinkao/controller/RestaurantController.java
@@ -5,9 +5,11 @@ import ku.kinkao.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class RestaurantController {
@@ -21,12 +23,15 @@ public class RestaurantController {
     }
 
     @GetMapping("/restaurant/add")
-    public String getAddPage() {
+    public String getAddPage(RestaurantRequest restaurantRequest) {
         return "restaurant-add";  // return restaurant-add.html
     }
 
     @PostMapping("/restaurant/add")
-    public String addRestaurant(@ModelAttribute RestaurantRequest restaurant, Model model) {
+    public String addRestaurant(@Valid RestaurantRequest restaurant, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "restaurant-add";
+
         restaurantService.create(restaurant);
         return "redirect:/restaurant";
     }
