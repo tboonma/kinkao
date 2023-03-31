beee20091f8c4e02c38c4731ff590bc999f4c205
tboonma <tboonma@hotmail.com>
2023-03-31 16:57:34 +0700

    Added Roles to User Data

--- src/main/java/ku/kinkao/dto/SignupDto.java
+++ src/main/java/ku/kinkao/dto/SignupDto.java
@@ -28,4 +28,9 @@ public class SignupDto {
     @Email
     @NotBlank
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(ROLE_ADMIN|ROLE_USER)$",
+            message = "Role is in an incorrect format.")
+    private String role;
 }
--- src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
+++ src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
@@ -3,12 +3,14 @@ package ku.kinkao.service;
 import ku.kinkao.model.Member;
 import ku.kinkao.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
@@ -17,14 +19,16 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username);
+        Member user = memberRepository.findByUsername(username);
 
-        if (member == null) {
+        if (user == null) {
             throw new UsernameNotFoundException("Could not find user");
         }
 
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+
         return new org.springframework.security.core.userdetails.User(
-                member.getUsername(), member.getPassword(),
-                new ArrayList<>());
+                user.getUsername(), user.getPassword(), authorities);
     }
 }
