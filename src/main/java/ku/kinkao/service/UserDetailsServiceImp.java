bc4a3ebd73cec9f68b6d89feaa926cc1621f2eed
tboonma <tboonma@hotmail.com>
2023-05-10 11:47:45 +0700

    Done Error Page, Logging & Monitoring

--- src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
+++ src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
@@ -2,6 +2,8 @@ package ku.kinkao.service;
 
 import ku.kinkao.model.Member;
 import ku.kinkao.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,11 +11,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImp.class);
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -27,6 +32,8 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority(user.getRole()));
+
+        logger.info(username + " has successfully logged in at " + Instant.now());
 
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(), user.getPassword(), authorities);
